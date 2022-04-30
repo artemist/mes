d1297688a8e010e58052743675a536488afea80b
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2022-04-30 22:51:42 +0200

    ARM: build: Cater for M2-Planet.
    * include/linux/arm/syscall.h: Add support for M2-Planet.
    * lib/linux/arm-mes-m2/_exit.c,
    lib/linux/arm-mes-m2/_write.c,
    lib/linux/arm-mes-m2/crt1.M1,
    lib/linux/arm-mes-m2/crt1.c,
    lib/linux/arm-mes-m2/syscall.c,
    lib/m2/arm/ELF-arm.hex2,
    lib/m2/arm/arm_defs.M1: New files.
    * build-aux/build.sh.in: Also allow kaem build for ARM.

--- include/linux/arm/syscall.h
+++ include/linux/arm/syscall.h
@@ -1,6 +1,6 @@
 /* -*-comment-start: "//";comment-end:""-*-
  * GNU Mes --- Maxwell Equations of Software
- * Copyright © 2018 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2018,2022 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
  * Copyright © 2020 Danny Milosavljevic <dannym@scratchpost.org>
  *
  * This file is part of GNU Mes.
@@ -23,29 +23,58 @@
 
 /* See https://github.com/torvalds/linux/blob/v4.19/arch/arm/tools/syscall.tbl */
 
-// libc
+/* libc-mini */
+#ifndef SYS_exit
+// CONSTANT SYS_exit 0x01
+#define SYS_exit    0x01
+#endif
+#ifndef SYS_write
+// CONSTANT SYS_write 0x04
+#define SYS_write   0x04
+#endif
+
+/* libc */
+// CONSTANT SYS_fork 0x02
 #define SYS_fork    0x02
+// CONSTANT SYS_read 0x03
 #define SYS_read    0x03
+// CONSTANT SYS_open 0x05
 #define SYS_open    0x05
 //#define SYS_waitpid
+// CONSTANT SYS_wait4 0x72
 #define SYS_wait4   0x72
+// CONSTANT SYS_execve 0x0b
 #define SYS_execve  0x0b
+// CONSTANT SYS_chmod 0x0f
 #define SYS_chmod   0x0f
+// CONSTANT SYS_access 0x21
 #define SYS_access  0x21
+// CONSTANT SYS_brk 0x2d
 #define SYS_brk     0x2d
+// CONSTANT SYS_ioctl 0x36
 #define SYS_ioctl   0x36
+// CONSTANT SYS_fsync 0x76
 #define SYS_fsync   0x76
+// CONSTANT SYS_getcwd 0xb7
+#define SYS_getcwd  0xb7
+// CONSTANT SYS_dup 0x29
+#define SYS_dup     0x29
+// CONSTANT SYS_dup2 0x3f
+#define SYS_dup2    0x3f
+// CONSTANT SYS_unlink 0x0a
+#define SYS_unlink  0x0a
+// CONSTANT SYS_gettimeofday 0x4e
+#define SYS_gettimeofday 0x4e
+// CONSTANT SYS_clock_gettime 0x107
+#define SYS_clock_gettime 0x107
 
-// libc+tcc
+/* libc+tcc */
 #define SYS_close  0x06
 #define SYS_lseek  0x13
-#define SYS_unlink 0x0a
 #define SYS_rmdir  0x28
-#define SYS_gettimeofday 0x4e
 #define SYS_stat   0x6a
-#define SYS_getcwd 0xb7
 
-// libc+gnu
+/* libc+gnu */
 #define SYS_chdir     0x0c
 #define SYS_link      0x09
 #define SYS_getpid    0x14
@@ -53,34 +82,31 @@
 #define SYS_kill      0x25
 #define SYS_rename    0x26
 #define SYS_mkdir     0x27
-#define SYS_dup       0x29
 #define SYS_pipe      0x2a
 #define SYS_getgid    0x2f
 #define SYS_rt_sigaction 0xae
 #define SYS_rt_sigreturn 0xad
 #define SYS_fcntl     0x37
-#define SYS_dup2      0x3f
 #define SYS_getrusage 0x4d
 #define SYS_lstat     0x6b
 #define SYS_setitimer 0x68
 #define SYS_fstat     0x6c
 #define SYS_nanosleep 0xa2
 #define SYS_getdents  0x8d
-#define SYS_clock_gettime 0x107
 
-// bash
+/* bash */
 #define SYS_setuid    0x17
 #define SYS_setgid    0x2e
 #define SYS_geteuid   0x31
 #define SYS_getegid   0x32
 #define SYS_getppid   0x40
 
-// make+WITH_GLIBC
+/* make+WITH_GLIBC */
 #define SYS_rt_sigprocmask 0xaf
 
-// tar
+/* tar */
 #define SYS_symlink   0x53
 #define SYS_readlink  0x55
 #define SYS_mknod     0x0e
 
-#endif // __MES_LINUX_ARM_SYSCALL_H
+#endif /* __MES_LINUX_ARM_SYSCALL_H */
